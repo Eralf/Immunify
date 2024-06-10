b79698d1a8a4c6ebd6d2f4492df742f9b55c4266
DominickAlbert <dominickalberd@gmail.com>
2024-06-11 00:35:26 +0700

    Finished Login and Vaccine Details

--- screens/VaccineDetailsScreen.js
+++ screens/VaccineDetailsScreen.js
@@ -1,38 +1,88 @@
 import React from 'react';
-import { View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Button, TouchableOpacity, StyleSheet, Dimensions, Image } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
-const VaccineDetailsScreen = ({ navigation, route }) => {
-  return (
-    <View style={styles.container}>
+const dw = Dimensions.get('window').width;
+const dh = Dimensions.get('window').height;
 
-      <Text style={styles.text}>Vaccine Details Screen</Text>
+const VaccineDetailsScreen = ({ navigation, route }) => (
+  <View style={styles.container}>
 
+    <LinearGradient
+      colors={['#FFFFFF', '#FFFFFF']}
+      style={styles.background} />
+
+    <View style={styles.container1}>
+      <Text style={styles.text}>Vaskin Hepatitis B</Text>
+      <Text style={styles.description}>Vaksin hepatitis B mengandung antigen permukaan virus hepatitis B (HBsAg) yang sudah dinonaktifkan. Vaksin ini bekerja dengan cara merangsang sistem kekebalan tubuh agar menghasilkan antibodi untuk melawan virus.</Text>
     </View>
-  );
-}
+
+    <TouchableOpacity style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>Daftar Sekarang</Text>
+    </TouchableOpacity>
+
+    <Image source={require('../assets/jumping-air.png')} style={styles.image} />
+
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+  },
+  container1: {
+    alignContent: 'center'
   },
   text: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 16,
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 25,
+    paddingHorizontal: 5
   },
-  button: {
+  description:{
+    textAlign: 'justify',
+    marginLeft: 30,
+    marginRight: 30,
+    fontFamily: 'NunitoSans-Medium',
+    width: dw/2 + 150,
+    paddingHorizontal: 5,
+    fontSize: 20,
+    fontWeight: 'light'
+  },
+  buttonContainer: {
     alignItems: 'center',
-    backgroundColor: '#9999FF',
-    padding: 10,
-    margin: 5,
-    borderRadius: 5
+    backgroundColor: '#E7FCFF',
+    borderRadius: 25,
+    paddingVertical: 10,
+    width: '50%',
+    justifyContent:'center',
+    alignContent: 'center',
   },
   buttonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  }
+    color: Colors.black,
+    fontWeight: 'light',
+    fontSize: 18,
+    width: 76,
+    textAlign:'center'
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: dh,
+  },
+  image: {
+    width: 320,
+    height: 320,
+    resizeMode: 'contain',
+    top: (dh/-20) + 50,
+  },
 });
 
 export default VaccineDetailsScreen;
